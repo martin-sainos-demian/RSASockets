4c4798ee31fd3ddbaebdcf1c70200348f2574736
martin-sainos-demian <martin.sainos.demian@gmail.com>
2022-11-11 01:07:15 -0600

    ya sirve

--- src/pkg/Launcher.java
+++ src/pkg/Launcher.java
@@ -9,9 +9,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class Launcher {     
-    static JButton keygenbtn;
+    static JButton keygenbtn, cipherbtn, decipherbtn;
     static RSA rsa;
     
     public static void main(String[] args) throws Exception {
@@ -23,28 +24,80 @@ public class Launcher {
         
         JPanel keygen=new JPanel();
         keygen.setLayout(new BoxLayout(keygen, BoxLayout.Y_AXIS));
-        String keygenstr="generar claves";
+        String keygenstr="  generar claves";
         JLabel keygenlabel = new JLabel(keygenstr);
-        JLabel keygenpublic = new JLabel(" ");
-        JLabel keygenprivate = new JLabel(" ");
+        JTextArea keygenpublictxt = new JTextArea(5, 20);
+        keygenpublictxt.setLineWrap(true);
+        keygenpublictxt.setWrapStyleWord(true);
+        keygenpublictxt.setEditable(false);
+        JLabel keygenpublic = new JLabel("publica: ");
+        JTextArea keygenprivatetxt = new JTextArea(5, 20);
+        keygenprivatetxt.setLineWrap(true);
+        keygenprivatetxt.setWrapStyleWord(true);
+        keygenprivatetxt.setEditable(false);
+        JLabel keygenprivate = new JLabel("privada: ");
         keygenbtn=new JButton("generar");
         keygenbtn.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
                 String[] keys=rsa.getKeys();
-                keygenpublic.setText("publica: "+keys[0]);
-                keygenprivate.setText("privada: "+keys[1]);
+                keygenpublictxt.setText(keys[0]);
+                keygenprivatetxt.setText(keys[1]);
             }
-
         });
         
         keygen.add(keygenlabel);
         keygen.add(keygenpublic);
+        keygen.add(keygenpublictxt);
         keygen.add(keygenprivate);
+        keygen.add(keygenprivatetxt);
         keygen.add(keygenbtn);
         
-        frame.getContentPane().add(keygen);
+        
+        JPanel cipher=new JPanel();
+        cipher.setLayout(new BoxLayout(cipher, BoxLayout.Y_AXIS));
+        JLabel cipherin = new JLabel("num a cifrar/num ya cifrado: ");
+        JTextArea cipherintxt = new JTextArea(5, 20);
+        cipherintxt.setLineWrap(true);
+        cipherintxt.setWrapStyleWord(true);
+        JLabel cipherkey = new JLabel("llave: ");
+        JTextArea cipherkeytxt = new JTextArea(5, 20);
+        cipherkeytxt.setLineWrap(true);
+        cipherkeytxt.setWrapStyleWord(true);
+        cipher.add(cipherin);
+        cipher.add(cipherintxt);
+        cipher.add(cipherkey);
+        cipher.add(cipherkeytxt);
+        
+        JLabel cipherres = new JLabel("resultado: ");
+        JTextArea cipherrestxt = new JTextArea(5, 20);
+        cipherrestxt.setLineWrap(true);
+        cipherrestxt.setWrapStyleWord(true);
+        cipherrestxt.setEditable(false);
+        cipherbtn=new JButton("cifrar");
+        cipherbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String res=rsa.cifrar(cipherintxt.getText(), cipherkeytxt.getText());
+                cipherrestxt.setText(res);
+            }
+        });
+        decipherbtn=new JButton("descifrar");
+        decipherbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String res=rsa.decifrar(cipherintxt.getText(), cipherkeytxt.getText());
+                cipherrestxt.setText(res);
+            }
+        });
+        cipher.add(cipherres);
+        cipher.add(cipherrestxt);
+        cipher.add(cipherbtn);
+        cipher.add(decipherbtn);
+        
+        
+        frame.getContentPane().add(BorderLayout.CENTER, keygen);
+        frame.getContentPane().add(BorderLayout.EAST, cipher);
         
         frame.setVisible(true); 
     }
--- src/pkg/RSA.java
+++ src/pkg/RSA.java
@@ -1,12 +1,25 @@
 
 package pkg;
 
+import java.io.StringReader;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Security;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class RSA {
@@ -40,5 +53,105 @@ public class RSA {
         }catch(Exception e){
             System.out.println("error "+e);
         }
+    }
+    
+    
+    public String cifrar(String text, String key){
+        String result="";
+        result=Base64.getEncoder().encodeToString(cipher(text, key));
+        return result;
+    }
+    private byte[] cipher(String text, String key){
+        try {
+            Cipher cifrado = Cipher.getInstance("RSA", "BC");
+            
+            if(text.length()<=64){
+                int num=Integer.parseInt(text);
+                byte[] bufferplano = BigInteger.valueOf(num).toByteArray();
+                //ciframos con publica
+                PublicKey clave= getKey(key);
+                cifrado.init(Cipher.ENCRYPT_MODE, clave);
+
+                System.out.println("3 ciframos con clave public: ");
+
+                byte[] buffercifrado = cifrado.doFinal(bufferplano);
+
+                System.out.println("txt cipher: ");
+                //no tiene format
+                mostrarBytes(buffercifrado);
+                System.out.println("");
+                return buffercifrado;
+            }
+        } catch (Exception e) {
+            System.out.println("error: "+e);
+        }
+        return null;
+    }
+    
+    public String decifrar(String text, String key){
+        String result="";
+        byte[] des=decipher(text, key);
+        int num=0;
+        for (byte b : des) {
+            num = (num << 8) + (b & 0xFF);
+        }
+        result=String.valueOf(num);
+        return result;
+    }
+    private byte[] decipher(String text, String key){
+        try {
+            Cipher cifrado = Cipher.getInstance("RSA", "BC");
+            
+            //desciframos con privada
+            PrivateKey clave= getKeyPriv(key);
+            cifrado.init(Cipher.DECRYPT_MODE, clave);
+
+            System.out.println("4 descipher con clave priv");
+
+            byte[] buffercifrado = Base64.getDecoder().decode(text);
+            byte[] bufferdescifrado = cifrado.doFinal(buffercifrado);
+
+            System.out.println("texto descifrado: ");
+            mostrarBytes(bufferdescifrado);
+
+            System.out.println("");
+            return bufferdescifrado;
+        } catch (Exception e) {
+            System.out.println("error: "+e);
+        }
+        return null;
+    }
+    
+    private static PublicKey getKey(String key){
+        try{
+            System.out.println("get key from "+key);
+            byte[] byteKey = Base64.getDecoder().decode(key);
+            X509EncodedKeySpec X509publicKey = new X509EncodedKeySpec(byteKey);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+
+            return kf.generatePublic(X509publicKey);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    private static PrivateKey getKeyPriv(String key){
+        try{
+            System.out.println("get key from "+key);
+            byte[] buffer = Base64.getDecoder().decode(key);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(buffer);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            return (PrivateKey) keyFactory.generatePrivate(keySpec);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    private static void mostrarBytes(byte[] buffer) {
+        System.out.write(buffer, 0, buffer.length);
     }
 }
